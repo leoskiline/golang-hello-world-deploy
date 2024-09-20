cdb83d5ddc91798b047b22cfe7b3ca520cf671f9
Leonardo Custodio <44417666+leoskiline@users.noreply.github.com>
2024-09-19 22:02:26 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-	http.HandleFunc("/", helloWorld) // Rota principal
-	fmt.Println("Servidor iniciado na porta 80...")
-	http.ListenAndServe(":80", nil) // Escuta na porta 80
+    http.HandleFunc("/", handler)
+    fmt.Println("Servidor HTTP rodando na porta 80")
+    err := http.ListenAndServe(":80", nil)
+    if err != nil {
+        fmt.Println("Erro ao iniciar o servidor:", err)
+    }
 }
